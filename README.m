f1da990df6b23bad93cccf0a24829b10bad0087b
Arunbalaji6768 <144259208+Arunbalaji6768@users.noreply.github.com>
2023-09-06 21:22:23 +0530

    Create README.m
    I am Arunbalaji from Tamilnadu interested in part time job in web developing . I am learning coding.

---
+++ README.m
@@ -0,0 +1,10 @@
+-  I’m @Arunbalaji6768
+- 👀 I’m interested in web developing 
+- 🌱 I’m currently learning 1st year of college 
+- 💞️ I’m looking to collaborate on software companies
+- 📫 How to reach me through my email id prarunbalaji853@gmail.com
+
+<!---
+Arunbalaji6768/Arunbalaji6768 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
