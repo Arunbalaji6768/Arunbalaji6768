6aada4d50f7246961e0efd3072ae8578fef93bbd
P. R. Arunbalaji <144259208+Arunbalaji6768@users.noreply.github.com>
2025-09-21 18:59:03 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,8 +1,8 @@
 -  I’m @Arunbalaji6768
-- 👀 I’m interested in web developing 
-- 🌱 I’m currently learning 2nd year of college 
-- 💞️ I’m looking to collaborate on software companies
+- 👀 I’m a AI powered Full Stack Developer
+- 🌱 I’m currently learning 3rd year of college 
 - 📫 How to reach me through my email id prarunbalaji853@gmail.com
+-  Linkedin-https://www.linkedin.com/in/arunbalaji-pr-881242288
 
 <!---
 Arunbalaji6768/Arunbalaji6768 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
